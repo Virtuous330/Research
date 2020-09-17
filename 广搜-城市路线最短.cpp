0007c40934d01772c112a97d40edd52081263146
T <71181168+Virtuous330@users.noreply.github.com>
2020-09-17 14:52:41 +0800

    Update 广搜-城市路线最短.cpp

--- 广搜-城市路线最短.cpp
+++ 广搜-城市路线最短.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cities[9][9] = {
+int ju[9][9] = {
 		
 							{0,0,0,0,0,0,0,0,0},
 							{0,1,0,0,0,1,0,1,1},
@@ -20,11 +20,12 @@ bool s[9];
 
 int out(int d)
 {
-	
+//	cout<<"A[d]="<<a[d]<<endl;
 	cout<<char(a[d] + 64);
+//	cout<<"==="<<endl;
 	while(b[d])
 	{
-		
+//		cout<<"b[d]="<<b[d]<<endl;
 		d = b[d];
 		cout<<"--"<<char(a[d] + 64);
 		
@@ -42,17 +43,71 @@ void doit()
 	a[1]=1;
 	b[1]=0;
 	s[1]=1;
+	do
+	{
+		
+		head++;
+		for(i = 1;i <= 8;i++){
+			
+//			cout<<i<<endl;
+			cout<<endl<<"================"<<endl<<"h: "<<a[head] + 1<<" l: "<<i + 1<<" ju:"<<ju[a[head]][i]<<endl<<"================"<<endl;
+			
+			if((ju[a[head]][i] == 0) && (s[i] == 0)){
+				
+//				cout<<"a[head]"<<a[head];
+				cout<<"tail="<<tail;
+				tail++;
+				a[tail]=i;
+				b[tail]=head;
+				s[i]=1;
+				if(i == 8){
+					
+					out(tail);
+					head = tail;
+					
+//					for(int ii = 0;ii < sizeof(a);ii++)
+//					{
+//						
+//						cout<<"a["<<ii<<"]="<<a[ii]<<",";
+//						
+//					}
+//	
+//					cout<<endl<<endl;
+//	
+//					for(int ii = 0;ii < sizeof(b);ii++)
+//					{
+//						
+//						cout<<"b["<<ii<<"]="<<b[ii]<<",";
+//						
+//					}
+//					
+//					cout<<endl<<endl;
+//					
+//					for(int ii = 0;ii < sizeof(s);ii++)
+//					{
+//						
+//						cout<<"s["<<ii<<"]="<<s[ii]<<",";
+//						
+//					}
+					
+					break;
+					
+				}
+				
+			}
+			
+		}
+		
+	}while(head < tail);
 	
 } 
 
 int main()
 {
 	
-	
-	
+	memset(s,false,sizeof(s));
+	doit();
 	
 	return 0;
 	
 } 
-
-
